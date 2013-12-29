88a347bc13cec5136bd7d64bf4be4839ce71d078
StoneField88 <uri.steinf@gmail.com>
2013-12-29 12:26:44 +0200

    brow.si

--- Groups.php
+++ Groups.php
@@ -43,5 +43,19 @@ and open the template in the editor.
                 </div>
             </div>
         </div>
+<script type="text/javascript">
+    (function(w, d){
+        w['_brSiteId'] = 'V9bLpo';
+        function br() {
+            var i='browsi-js'; if (d.getElementById(i)) {return;}
+            var siteId = /^[a-zA-Z0-9]{1,7}$/.test(w['_brSiteId']) ? w['_brSiteId'] : null;
+            var js=d.createElement('script'); js.id=i; js.async=true;
+            js.src='//js.brow.si/' + ( siteId != null ? siteId + '/' : '' ) + 'br.js';
+            (d.head || d.getElementsByTagName('head')[0]).appendChild(js);
+        }
+        d.readyState == 'complete' ? br() :
+            ( w.addEventListener ? w.addEventListener('load', br, false) : w.attachEvent('onload', br) );
+    })(window, document);
+</script>
     </body>
 </html>
