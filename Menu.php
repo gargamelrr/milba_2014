875ff56774f41292208ffc0b54e1cf1d3b9b8c86
gargamelrr <ringelronny@gmail.com>
2013-12-30 17:41:05 +0200

    back to html

--- Menu.php
+++
@@ -1,7 +0,0 @@
-            <div data-role="panel" data-position="left" data-position-fixed="false" data-display="reveal" id="nav-panel" data-theme="a">
-                        <a href="index.php" data-role="button" data-theme="e" data-icon="home" > Home </a><br/>
-                        <a href="NewTask.php" data-role="button" data-theme="b" data-icon="plus" > Create </a><br/>
-                        <a href="JoinGroups.php" data-role="button" data-theme="b" data-icon="search" > Join </a><br/>
-                        <a href="Groups.php" data-role="button"  data-theme="b" data-icon="grid" > Groups </a><br/>
-                        <a href="Settings.php" data-role="button"  data-theme="b" data-icon="gear" > Settings </a><br/>
-            </div>
