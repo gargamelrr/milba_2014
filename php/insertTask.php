dee7606a332b1f50a177b0b367636adbc2acb2e7
StoneField88 <uri.steinf@gmail.com>
2014-02-23 14:45:24 +0200

    insert task

--- php/insertTask.php
+++ php/insertTask.php
@@ -6,8 +6,8 @@ require_once __DIR__ . '/db_connect.php';
 // connecting to db
 $db = new DB_CONNECT();
 
-if (isset($_POST["taskName"]) && isset($_POST["date1"])&& isset($_POST["taskTime"])
-        && isset($_POST["radiodifficulty:"])&& isset($_POST["taskdetails"])) {
+if (isset($_POST["taskName"]) && isset($_POST["date1"]) && isset($_POST["taskTime"])
+        && isset($_POST["radiodifficulty"]) && isset($_POST["taskdetails"])) {
 
    $response = array();
     $taskName = $_POST['taskName'];
@@ -17,8 +17,15 @@ if (isset($_POST["taskName"]) && isset($_POST["date1"])&& isset($_POST["taskTime
     $taskDetails = $_POST['taskdetails'];
     $date = date("Y-m-d");
     
-    $result = mysql_query("INSERT INTO `Tasks`(`index`, `course_id`, `name`, `due_date`, `description`, `creator`, `status`, `created`) "
-            . "VALUES ('11', '11', '$taskName', '$dueDate . $taskTime', '$taskDetails', 'Ronny', '111', '$date')");
+    //SQL column for difficulty is boolean, ajust it. 
+    if($radioDifficulty == "hard") {
+        $radioDifficulty = 1;
+    } else {
+        $radioDifficulty = 0;
+    }
+    
+    $result = mysql_query("INSERT INTO `Tasks`(`index`, `course_id`, `name`, `due_date`, `description`, `difficulty`, `creator`, `status`, `created`) "
+            . "VALUES ('1111', '11', '$taskName', '$dueDate . $taskTime', '$taskDetails', '$radioDifficulty', 'Ronny', '111', '$date')");
     
 // check if row inserted or not
     if ($result) {
