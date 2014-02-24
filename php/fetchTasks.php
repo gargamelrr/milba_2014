7c584ee1c622d93594faf464d55d2775d124e4a5
StoneField88 <uri.steinf@gmail.com>
2014-02-24 19:03:41 +0200

    עגכעכגעכג

---
+++ php/fetchTasks.php
@@ -0,0 +1,35 @@
+<?php
+
+$response = array();
+
+require_once __DIR__ . '/db_connect.php';
+
+$db = new DB_CONNECT();
+
+$result = mysql_query("SELECT name, due_date FROM `Tasks`") or die(mysql_error());
+
+if (mysql_num_rows($result) > 0) {
+    
+    $response["allTasks"] = array();
+
+    while ($row = mysql_fetch_array($result)) {
+        
+        $task = array();
+        $task["name"] = $row["name"];
+        $task["due_date"] = $row["due_date"];
+        array_push($response["allTasks"], $task);
+        
+    }
+
+    $response["success"] = 1;
+
+    echo json_encode($response);
+    
+} else {
+    
+    $response["success"] = 0;
+    $response["message"] = "No tasks found";
+
+    echo json_encode($response);
+}
+?>
