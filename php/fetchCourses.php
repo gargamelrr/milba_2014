28004c2e48b3f9a4ea925270c9ded3ca2a1c0a38
StoneField88 <uri.steinf@gmail.com>
2014-02-26 20:27:46 +0200

    dsdsd

---
+++ php/fetchCourses.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/db_connect.php';
+
+$db = new DB_CONNECT();
+
+//User is hard coded now.. look for PHP session later
+$result = mysql_query("SELECT name FROM `Courses` , `Users_Courses` where Users_Courses.course_id = Courses.index and student_id=1") or die(mysql_error());
+
+if (mysql_num_rows($result) > 0) {
+    
+    $response["userCourses"] = array();
+
+    while ($row = mysql_fetch_array($result)) {
+        
+        $course = array();
+        $task["name"] = $row["name"];
+        array_push($response["userCourses"], $task);
+        
+    }
+
+    $response["success"] = 1;
+
+    echo json_encode($response);
+    
+} else {
+    
+    $response["success"] = 0;
+    $response["message"] = "No tasks found";
+
+    echo json_encode($response);
+}
+?>
