061639da1299f63b72495115291fdb4635ac557a
gargamelrr <ringelronny@gmail.com>
2014-03-01 17:57:44 +0200

    fdsds

--- php/fetchTasks.php
+++ php/fetchTasks.php
@@ -6,30 +6,25 @@ require_once __DIR__ . '/db_connect.php';
 
 $db = new DB_CONNECT();
 $courseID = $_GET['courseID'];
-$result = mysql_query("SELECT name, due_date FROM `Tasks` Where course_id = '$courseID' ") or die(mysql_error());
+$result = mysql_query("SELECT name, due_date FROM `Tasks` Where course_id = '$courseID' order by due_date");
 
 if (mysql_num_rows($result) > 0) {
-    
+
     $response["allTasks"] = array();
 
     while ($row = mysql_fetch_array($result)) {
-        
+
         $task = array();
         $task["name"] = $row["name"];
         $task["due_date"] = $row["due_date"];
         array_push($response["allTasks"], $task);
-        
     }
 
     $response["success"] = 1;
-
-    echo json_encode($response);
-    
 } else {
-    
+
     $response["success"] = 0;
     $response["message"] = "No tasks found";
-
-    echo json_encode($response);
 }
+echo json_encode($response);
 ?>
