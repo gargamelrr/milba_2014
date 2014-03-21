31c890ae9f270e2fa991b5c4b87d408a51a18f48
gargamelrr <ringelronny@gmail.com>
2014-03-21 13:26:35 +0200

    fdsfs

--- php/userActions.php
+++ php/userActions.php
@@ -17,6 +17,7 @@ $fb_id = $_POST["fb_id"];
 
 //check if user exist
 $result = mysql_query("select `index`,first_name,last_name,email from Users where fb_id='$fb_id'");
+$count = mysql_num_rows($result);
 if (mysql_num_rows($result) > 0) {
     $row = mysql_fetch_array($result);
     $id = $row["index"];
@@ -44,8 +45,8 @@ $_SESSION["user"] = $user;
 $_SESSION["name"] = $name;
 $_SESSION["user_id"] = $id;
 
-$response["debug1"] = mysql_error();
-$response["debug"] = $_SESSION["user"] . " " . $_SESSION["user_id"];
+$response["debug1"] = mysql_error() . " " . $fb_id;
+//$response["debug"] = $_SESSION["user"] . " " . $_SESSION["user_id"];
 $response["success"] = "1";
 
 echo json_encode($response);
