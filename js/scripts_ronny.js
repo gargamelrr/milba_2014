6bbd541bfc3597ef6925b82b7b59a06d36a6b7bf
gargamelrr <ringelronny@gmail.com>
2014-04-16 19:08:14 +0300

    fdsfds

--- js/scripts_ronny.js
+++ js/scripts_ronny.js
@@ -219,7 +219,7 @@ function tokenHandler(result) {
 }
 
 function onNotificationGCM(e) {
-
+    alert(e);
     switch (e.event)
     {
         case 'registered':
@@ -227,7 +227,7 @@ function onNotificationGCM(e) {
             {
                 // Your GCM push server needs to know the regID before it can push to this device
                 // here is where you might want to send it the regID for later use.
-                //alert("regID = " + e.regid);
+                alert("regID = " + e.regid);
                 setGCM(e.regid);
             }
             break;
