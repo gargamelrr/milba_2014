5847fc4e131fde88265b3ac28eaa5485fb02529b
gargamelrr <ringelronny@gmail.com>
2014-04-29 18:40:12 +0300

    gdfgfd

--- js/scripts_ronny.js
+++ js/scripts_ronny.js
@@ -367,16 +367,12 @@ document.addEventListener('deviceready', function() {
 }, false);
 
 function invite() {
-    try{
 FB.ui({method: 'apprequests',
         message: 'YOUR_MESSAGE_HERE',
         to: 'USER_ID'
     }, requestCallback);
-    }catch (e){
-        alert(e);
-    }
 }
 
 function requestCallback(response) {
-    alert("test " + response);
+    alert("test " + response.to);
 }
