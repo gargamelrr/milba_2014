1f6348cb8e936add1c219ece113c10569a60f334
gargamelrr <ringelronny@gmail.com>
2014-04-30 09:33:02 +0300

    gdfg

--- js/scripts_uri.js
+++ js/scripts_uri.js
@@ -258,7 +258,7 @@ $(document).on("pageshow", "#courses", function() {
         url: 'http://ronnyuri.milab.idc.ac.il/milab_2014/php/fetchCourses.php',
         method: 'POST',
         success: function(data) {
-            //alert("debug-ignore: " + data);
+            alert(data);
             var json = JSON.parse(data);
             if (json.success == 1) {
                 createCoursesButtons(json.userCourses, "coursesMy");
